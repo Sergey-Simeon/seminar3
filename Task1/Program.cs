6d0886c44af6080f45b6b60ee8f6e5eaadf835fb
Sergey <sergei.simeon@yandex.ru>
2022-12-11 12:15:19 +0300

    Задание 1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Реализовать через функции.
+int Phrase(string message)
+
+{
+    System.Console.Write($"{message} > ");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+int a = Phrase ("Введите число А ");
+int b = Phrase ("Введите число B ");
+int result = a;
+
+if (a > 0 && b > 0)
+{
+
+for (int n = 1; n < b; n++)
+{
+    result = result * a;
+}
+Console.Write($"Результат возведения в степень = {result}");
+}
+else
+{
+    Console.Write("Вы ввели ненатуральное число");
+}
